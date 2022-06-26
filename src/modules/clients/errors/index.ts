abb6c31fb2fea37e641d4317068f8564ac431afb
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-06-26 15:47:41 -0300

    feat: add export error

--- src/modules/clients/errors/index.ts
+++ src/modules/clients/errors/index.ts
@@ -1,3 +1,4 @@
 export { default as MissingParamError } from "./MissingParamError";
 export { default as UserNotFound } from "./UserNotFound";
 export { default as UserAlreadyExists } from "./UserAlreadyExists";
+export { default as CredentialError } from "./CredentialError";
