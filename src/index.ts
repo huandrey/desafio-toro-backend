c896d4fe55b730f8fc8cf7d36821fc5b2165bc5c
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-06-26 18:39:18 -0300

    feat: add transaction routes in server

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import "reflect-metadata";
 import "./database";
 import { accountRoutes } from "./routes/accounts.routes";
 import { authenticateRoutes } from "./routes/authenticate.routes";
+import { transactionRoutes } from "./routes/transactions.routes";
 import { userRoutes } from "./routes/users.routes";
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(express.json());
 app.use("/session", authenticateRoutes);
 app.use("/users", userRoutes);
 app.use("/account", accountRoutes);
-
+app.use("/spb/events", transactionRoutes);
 app.listen(8080, () => {
   console.log("Server is running in PORT 8080");
 });
