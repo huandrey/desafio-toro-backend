d926bdeb4ec9b4809288a4cdfd7cd6c7899321db
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-06-26 15:50:58 -0300

    feat: import the routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 
+import "reflect-metadata";
 import "./database";
+import { accountRoutes } from "./routes/accounts.routes";
+import { authenticateRoutes } from "./routes/authenticate.routes";
 import { userRoutes } from "./routes/users.routes";
 
 const app = express();
 app.use(express.json());
 
+app.use("/session", authenticateRoutes);
 app.use("/users", userRoutes);
+app.use("/account", accountRoutes);
 
 app.listen(8080, () => {
   console.log("Server is running in PORT 8080");
