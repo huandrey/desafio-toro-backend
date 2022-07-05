43e1712034f9f98738acb7ca8c2874a0da3f588b
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-05 01:08:11 -0300

    feat: implement middle for handle errors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import express from "express";
+import "express-async-errors";
 
 import "reflect-metadata";
 import "./database";
 import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
 
 import { accountRoutes } from "./routes/accounts.routes";
 import { authenticateRoutes } from "./routes/authenticate.routes";
 import { transactionRoutes } from "./routes/transactions.routes";
 import { userRoutes } from "./routes/users.routes";
+import { AppError } from "./shared/errors/AppError";
 
 const app = express();
 app.use(express.json());
@@ -17,6 +19,20 @@ app.use("/users", userRoutes);
 app.use("/account", accountRoutes);
 app.use("/spb/events", transactionRoutes);
 
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
+
 app.listen(3333, () => {
-  console.log("Server is running in PORT 8080");
+  console.log("Server is running in PORT 3333");
 });
