70db631f276f440e7e2086ca01d1193b455337c7
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-04 22:09:25 -0300

    feat: add auth to account routes

--- src/routes/accounts.routes.ts
+++ src/routes/accounts.routes.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
 
 import { CreateAccountController } from "../modules/accounts/useCases/CreateAccount/CreateAccountController";
+import { GetAccountController } from "../modules/accounts/useCases/GetAccount/GetAccountController";
+import { ensureAuthenticated } from "../shared/http/middlewares/ensuredAutheticated";
 
 const accountRoutes = Router();
 
 const createAccountController = new CreateAccountController();
+const getAccountController = new GetAccountController();
 
-accountRoutes.post("/:userId", createAccountController.handle);
+accountRoutes.post(
+  "/:userId",
+  ensureAuthenticated,
+  createAccountController.handle
+);
+
+accountRoutes.get("/:userId", ensureAuthenticated, getAccountController.handle);
 
 export { accountRoutes };
