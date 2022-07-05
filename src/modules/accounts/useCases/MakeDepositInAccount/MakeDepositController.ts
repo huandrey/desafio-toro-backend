b853d06fc485b61c157003659e88bd7bf95233f2
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-04 22:09:51 -0300

    feat: add deposit feat

---
+++ src/modules/accounts/useCases/MakeDepositInAccount/MakeDepositController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+
+import { UserRepository } from "../../../clients/repositories/UserRepository";
+// import { UserService } from "../../../clients/services/UserService";
+import { AccountRepository } from "../../repositories/AccountRepository";
+import { AccountService } from "../../services/AccountService";
+import { MakeDepositUseCase } from "./MakeDepositUseCase";
+
+const userRep = new UserRepository();
+// const userService = new UserService(userRep);
+const accountRep = new AccountRepository();
+const accountServive = new AccountService(accountRep, userRep);
+
+export class CreateAccountController {
+  async handle(request: Request, response: Response) {
+    const { userId } = request.params;
+
+    const makeDepositUseCase = new MakeDepositUseCase(accountServive);
+
+    const res = await makeDepositUseCase.execute(userId);
+
+    return response.status(res.statusCode).json(res);
+  }
+}
