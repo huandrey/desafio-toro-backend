bfcd4c0b65b3674fba642c1301d05bc029d80eff
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-04 22:11:31 -0300

    feat: add cors to server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 import "reflect-metadata";
 import "./database";
+import cors from "cors";
+
 import { accountRoutes } from "./routes/accounts.routes";
 import { authenticateRoutes } from "./routes/authenticate.routes";
 import { transactionRoutes } from "./routes/transactions.routes";
@@ -9,11 +11,12 @@ import { userRoutes } from "./routes/users.routes";
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/session", authenticateRoutes);
 app.use("/users", userRoutes);
 app.use("/account", accountRoutes);
 app.use("/spb/events", transactionRoutes);
-app.listen(8080, () => {
+
+app.listen(3333, () => {
   console.log("Server is running in PORT 8080");
 });
