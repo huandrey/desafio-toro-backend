55c6aeb7143912b77812ccb7ee653ae15e7bc506
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-04 22:05:44 -0300

    refactor: function of the generate account number

--- src/modules/accounts/services/AccountService.ts
+++ src/modules/accounts/services/AccountService.ts
@@ -22,8 +22,8 @@ export class AccountService {
   }
 
   generateNumber() {
-    const max = 60;
-    const min = 1;
+    const max = 9;
+    const min = 0;
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
