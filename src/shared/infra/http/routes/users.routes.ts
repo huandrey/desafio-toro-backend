f6676c0e3b4776a19b2c2f51b442914fb8edc9a6
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:36:09 -0300

    feat: add route for recovery user data

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,10 +1,19 @@
 import { CreateUserController } from "@modules/clients/useCases/CreateUser/CreateUserController";
+import { RecoveryUserController } from "@modules/clients/useCases/RecoveryUser/RecoveryUseController";
 import { Router } from "express";
+
+import { ensureAuthenticated } from "../middlewares/ensuredAutheticated";
 
 const userRoutes = Router();
 
 const createUserController = new CreateUserController();
+const recoveryUserController = new RecoveryUserController();
 
 userRoutes.post("/", createUserController.handle);
+userRoutes.post(
+  "/recovery-data",
+  ensureAuthenticated,
+  recoveryUserController.handle
+);
 
 export { userRoutes };
