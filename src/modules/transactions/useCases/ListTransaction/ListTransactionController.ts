258e2c290e72b868709fa6021125003f125b8958
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:48:32 -0300

    refactor: change props of the constructor in list transaction

--- src/modules/transactions/useCases/ListTransaction/ListTransactionController.ts
+++ src/modules/transactions/useCases/ListTransaction/ListTransactionController.ts
@@ -2,18 +2,17 @@ import { Request, Response } from "express";
 
 import { AccountRepository } from "../../../accounts/repositories/AccountRepository";
 import { TransactionRepository } from "../../repositories/TransactionRepository";
-import { TransactionService } from "../../services/TransactionService";
 import { ListTransactionUseCase } from "./ListTransactionUseCase";
 
 const accountRep = new AccountRepository();
 const transactionRep = new TransactionRepository();
-const transactionService = new TransactionService(transactionRep, accountRep);
 
 export class ListTransactionController {
   async handle(request: Request, response: Response) {
     const { id } = request.user;
     const listTransactionUseCase = new ListTransactionUseCase(
-      transactionService
+      accountRep,
+      transactionRep
     );
 
     const res = await listTransactionUseCase.execute(id);
--- src/modules/transactions/useCases/ListTransaction/ListTransactionUseCase.ts
+++ src/modules/transactions/useCases/ListTransaction/ListTransactionUseCase.ts
@@ -1,20 +1,32 @@
+import { AccountRepository } from "@modules/accounts/repositories/AccountRepository";
+import { TransactionRepository } from "@modules/transactions/repositories/TransactionRepository";
+
+import { AppError } from "@shared/errors/AppError";
+
 import {
   badRequest,
   getSuccessRequest,
 } from "../../../clients/helpers/http-helper";
-import { TransactionService } from "../../services/TransactionService";
 
 export class ListTransactionUseCase {
-  constructor(private transactionService: TransactionService) {}
+  constructor(
+    private accountRep: AccountRepository,
+    private transactionRep: TransactionRepository
+  ) {}
 
   async execute(id: string) {
-    try {
-      const transactions = await this.transactionService.listTransactions(id);
-      return getSuccessRequest("Success list transactions", {
-        transactions,
-      });
-    } catch (err) {
-      return badRequest("Some error occured while list accounts", 500);
+    const account = await this.accountRep.findAccountByUserId(id);
+
+    if (!account) {
+      throw new AppError("User not has account!");
     }
+
+    const transactions = await this.transactionRep.getAllTransactionsOfUser(
+      account.id
+    );
+
+    return getSuccessRequest("Success list transactions", {
+      transactions,
+    });
   }
 }
